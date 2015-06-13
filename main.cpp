5ad93f54ba45d7193421cfc4d71f2e0d621d175f
root <root@hamidreza-pc.cmpe.ubc.ca>
2015-06-12 22:26:33 -0400

    version 1.0

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: root
+ *
+ * Created on June 12, 2015, 10:14 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
